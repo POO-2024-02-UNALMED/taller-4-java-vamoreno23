a9d6cf1db0d2cd657178449773b0e348a51b2bd0
vamoreno23 <152014036+vamoreno23@users.noreply.github.com>
2024-11-26 01:15:54 -0500

    Prueba 2

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -5,10 +5,9 @@ public class Persona {
     final long cedula;
     String nombre;
     static int totalPersonas;
-    
+
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
 
     public Persona(long cedula, String nombre) {
@@ -16,7 +15,7 @@ public class Persona {
         this.nombre = nombre;
         totalPersonas++;
     }
-    
+
     public Persona(String nombre, long cedula) {
         this.cedula = cedula;
         this.nombre = nombre;
@@ -30,10 +29,15 @@ public class Persona {
     }
 
     public Persona(String nombre) {
-        this.nombre = "";
+        this.cedula = 1;
+        this.nombre = nombre;
         totalPersonas++;
     }
-    
+
+    public Persona() {
+        this.cedula = 0;
+    }
+
     public long getCedula() {
         return cedula;
     }
